bf5a4f888ff7cce8096db7ba4bf4a9162a48d459
imWildCat <wildcat.name@gmail.com>
2018-07-08 19:12:36 +0100

    Add long speech recgnition

--- aitk/cloud/tencent/speech.py
+++ aitk/cloud/tencent/speech.py
@@ -12,6 +12,12 @@ FILE_TYPES_TO_CODE = {
     'silk': 4,
 }
 
+TEXT_TO_SPEECH_FORMATS = {
+    'pcm': 1,
+    'wav': 2,
+    'mp3': 3,
+}
+
 
 def check_extension(ext):
     if ext.lower() not in SUPPORTED_FILE_TYPES:
@@ -52,18 +58,20 @@ class TencentSpeech(object):
         # instead of 'utf-8'
         return json_res
 
-    def recognize_speech_long(self, speech=None, speech_url=None, format=None,
-                              callback_url=None, bit_rate=16000):
+    def recognize_speech_long(self, speech=None, speech_url=None,
+                              audio_format=None, callback_url=None,
+                              bit_rate=16000):
         assert speech or speech_url, \
             'Either local speech file or speech_url must be provided'
         assert callback_url, 'callback_url must be provided'
 
         if speech_url:
-            assert format, 'format mush be provided while using speech_url'
-            check_extension(format)
+            assert audio_format, \
+                'format mush be provided while using speech_url'
+            check_extension(audio_format)
             data = {
                 'speech_url': speech_url,
-                'format': FILE_TYPES_TO_CODE[format]
+                'format': FILE_TYPES_TO_CODE[audio_format]
             }
         else:
             f_type, _ = check_file_type(speech)
@@ -79,5 +87,32 @@ class TencentSpeech(object):
             '/aai/aai_wxasrlong',
             data,
         )
+
+        return json_res
+
+    def text_to_speech(self, text, speed=100, volume=0, audio_format='mp3',
+                       speaker=1, ant=0, apc=58):
+        assert text, 'text cannot be None'
+        assert len(text) > 0, 'text length must be larger than 0'
+        assert speed >= 50 and speed <= 200, \
+            'speed must be in range between 50 and 200'
+        assert volume >= -10 and volume <= 10, \
+            'volume must be in range between -10 and 10'
+        assert audio_format.lower() in TEXT_TO_SPEECH_FORMATS \
+            .keys(), 'format should be mp3, wav or pcm'
+        assert speaker in [1, 5, 6, 7], \
+            'speaker must be 1, 5, 6 or 7 as defined in Tencent documentation'
+        assert ant >= -24 and ant <= 24, 'ant must be in [-24, 24]'
+        assert apc >= 0 and apc <= 100, 'apc must be in [0, 100]'
+
+        json_res = self.client.http_post('/aai/aai_tts', {
+            'speaker': speaker,
+            'format': TEXT_TO_SPEECH_FORMATS[audio_format.lower()],
+            'volume': volume,
+            'speed': speed,
+            'text': text.encode('utf-8'),
+            'aht': ant,
+            'apc': apc,
+        })
 
         return json_res
