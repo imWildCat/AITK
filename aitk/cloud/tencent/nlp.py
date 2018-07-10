07f7b4f67548f46f55888c5626c7bf951267422b
imWildCat <wildcat.name@gmail.com>
2018-07-10 16:01:40 +0100

    Add translate API for tencent

--- aitk/cloud/tencent/nlp.py
+++ aitk/cloud/tencent/nlp.py
@@ -11,3 +11,19 @@ class TencentNLP(object):
         # This stupid issue is because the Tencent AI team is using 'gbk'
         # instead of 'utf-8'
         return json_res
+
+    def translate(self, text=None, source=None, target=None):
+        assert text, 'text must be provided'
+        assert source, 'source must be provided'
+        assert target, 'target must be provided'
+
+        json_res = self.client.http_post(
+            'nlp/nlp_texttranslate',
+            {
+                'text': text,
+                'source': source,
+                'target': target
+            }
+        )
+
+        return json_res
